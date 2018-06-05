8917684a813d301d629d23e0d0dcdd1d4b1dbd34
Jesús Romero <36158301+JRomero28@users.noreply.github.com>
2018-06-05 14:30:40 +0200

    Update ejer5.py

--- ejer5.py
+++ ejer5.py
@@ -1,8 +1,86 @@
-#Muestra las películas del director “Tarantino”
+#Ejercicio libre. Una película según id, director, género o año
 import json
 from pprint import pprint
 with open("peliculas.json") as data_file:
 	data=json.load(data_file)
-for bloque in data["peliculas"]:
-	if bloque["director"] == "Tarantino":
-		print("Título de la pelicula de Trantino:",bloque["nombre"])
+def menu(orden)
+	print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+	print("1. Por id")
+	print("2. Por direcotor")
+	print("3. Por género")
+	print("4. Por año")
+	print("5. Salir")
+	orden=int(input("¿Cómo quieres buscar la pelicula? "))
+
+while orden != 5:
+
+	if orden == 1:
+		ide=int(input("Número de la id (del 1 al 10): "))
+		print(" ")
+		for bloque in data["peliculas"]:
+			if bloque["id"] == ide:
+				print("Título de la pelicula de la id",ide,":",bloque["nombre"])
+				print(" ")
+				print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+				print("1. Por id")
+				print("2. Por direcotor")
+				print("3. Por género")
+				print("4. Por año")
+				print("5. Salir")
+				orden=int(input("¿Cómo quieres buscar la pelicula? "))
+
+	elif orden == 2:
+		director=input("Nombre del director: ")
+		print(" ")
+		for bloque in data["peliculas"]:
+			if bloque["director"] == director:
+				print("Título de la pelicula del director",director,":",bloque["nombre"])
+				print(" ")
+				print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+				print("1. Por id")
+				print("2. Por direcotor")
+				print("3. Por género")
+				print("4. Por año")
+				print("5. Salir")
+				orden=int(input("¿Cómo quieres buscar la pelicula? "))
+
+	elif orden == 3:
+		clasificacion=input("Nombre de la clasificacion: ")
+		print(" ")
+		for bloque in data["peliculas"]:
+			if bloque["director"] == clasificacion:
+				print("Título de la pelicula del genero",clasificacion,":",bloque["nombre"])
+				print(" ")
+				print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+				print("1. Por id")
+				print("2. Por direcotor")
+				print("3. Por género")
+				print("4. Por año")
+				print("5. Salir")
+				orden=int(input("¿Cómo quieres buscar la pelicula? "))
+
+	elif orden == 4:
+		año=int(input("Numero del año: "))
+		print(" ")
+		for bloque in data["peliculas"]:
+			if bloque["director"] == director:
+				print("Título de la pelicula del director",director,":",bloque["nombre"])
+				print(" ")
+				print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+				print("1. Por id")
+				print("2. Por direcotor")
+				print("3. Por género")
+				print("4. Por año")
+				print("5. Salir")
+				orden=int(input("¿Cómo quieres buscar la pelicula? "))
+
+	else:
+		print("ERROR: No has introducido el número correcto")
+		print(" ")
+		print("PULSA EL NÚMERO PARA LA BUSQUEDA")
+		print("1. Por id")
+		print("2. Por direcotor")
+		print("3. Por género")
+		print("4. Por año")
+		print("5. Salir")
+orden=int(input("¿Cómo quieres buscar la pelicula? "))
