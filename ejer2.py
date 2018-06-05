03d3ed1c4c511e9b401c6f79411c629355cd3739
Jesús Romero <36158301+JRomero28@users.noreply.github.com>
2018-06-05 14:28:08 +0200

    Update ejer2.py

--- ejer2.py
+++ ejer2.py
@@ -1,9 +1,11 @@
-#Muestra todos los directores de las películas
+#Contar la información. Todos los directores de las películas
 import json
 from pprint import pprint
 with open("peliculas.json") as data_file:
 	data=json.load(data_file)
+lista=[]
 contador=0
 for director in data["peliculas"]:
 	contador=contador+1
-	print("Nombre del director",contador,":",director["director"])
+	lista.append(director)
+print(len(lista), "directores en total")
